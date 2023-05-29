d4195bfe9d6033c6bb2e4d17295b20dfa02f8e3d
jmganotice15 <jmganotice16@gmail.com>
2023-05-29 15:24:26 +0800

    activity 2: combine fname, mname, lname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def full_name(fname)
+  return fname[:first_name]+' '+fname[:middle_name]+" "+fname[:last_name]
+end
+
+name = {first_name: 'JM', middle_name: 'Aquino', last_name: 'Ganotice'}
+
+p full_name(name)
+
+
